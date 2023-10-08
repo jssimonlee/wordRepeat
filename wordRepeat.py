daeaa7487eb6ebaed97cf80d3823876b128c940e
Sim Jae-Seok <jssimonlee@gmail.com>
2023-10-08 13:58:38 +0900

    Update wordRepeat.py
    submitted 버튼 위치 수정

--- wordRepeat.py
+++ wordRepeat.py
@@ -106,11 +106,12 @@ with tab3:
                 st.session_state["button1"] = False
             if "button2" not in st.session_state:
                 st.session_state["button2"] = False
-            if st.form_submit_button("삭제"):
+            submitted1 = st.form_submit_button("삭제")
+            if submitted1:
                 st.session_state["button1"] = not st.session_state["button1"]
             if st.session_state["button1"]:
-                sumitted1 = st.form_submit_button(f'"{selected_file}" 이 파일을 정말로 삭제하시겠습니까?')
-                if sumitted1:
+                submitted2 = st.form_submit_button(f'"{selected_file}" 이 파일을 정말로 삭제하시겠습니까?')
+                if submitted2:
                     st.session_state["button2"] = not st.session_state["button2"]
             if st.session_state["button2"]:
                 os.remove(selected_file)
