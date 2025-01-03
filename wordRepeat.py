34a020d7dba679e354b9a19b26c91fa70b2d8d6a
Sim Jae-Seok <jssimonlee@gmail.com>
2025-01-03 13:07:45 +0900

    Update wordRepeat.py

--- wordRepeat.py
+++ wordRepeat.py
@@ -198,7 +198,7 @@ with tab1:
             if extension.replace('.','') in extList:
                 if file != 'requirements.txt':
                     file_list_wanted.append(file)
-        file_list_wanted.sort()
+        file_list_wanted.sort().reverse()
         # 초기선택파일을 ini파일에서 읽어옴
         try:
             with open("initFile.ini","r",encoding="utf-8") as f:
